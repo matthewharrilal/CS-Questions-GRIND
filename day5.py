eac6b62326f94cd329b48a319149cc60a9bc21d9
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-16 22:50:16 -0800

    First iteration not complete of lemonade change

--- day5.py
+++ day5.py
@@ -30,3 +30,50 @@ def findTheDifference(s, t):
   res = tt-ss
   return res.keys()[0]
 print(findTheDifference(s = "abcd",t = "abcde"))
+
+
+def lemonadeChange(bills):
+  """
+  :type bills: List[int]
+  :rtype: bool
+  """
+
+  # EVERYONE BUYS OONLY ONE LEMONADE
+
+  # ({10: 2, 20: 1, 5: 2}, [5, 5, 10, 10, 20])
+
+  # DO WE EVEN CARE ABOUT PROFIT JUUST THE BILLS WE ARE COLLECTING
+
+  # Decrement profit from freq and increment bill freq
+
+  bill_freq = {} # Frequency of the bills
+  lemonade_price = 5 # Cost of 1 lemonade
+  
+  for bill in bills:
+    profit = (bill - lemonade_price)
+    if lemonade_price == bill:
+      if bill not in bill_freq:
+
+        bill_freq[bill] = 1
+
+      else:
+        bill_freq[bill] += 1
+    
+    else:
+      if bill in bill_freq:
+        bill_freq[bill] += 1
+      else:
+        bill_freq[bill] = 1
+      # PROFIT == BILL WE ARE DECREMENTING FREQ FROM
+      if profit in bill_freq:
+        bill_freq[profit] -= 1
+      else:
+        bill_freq[bill] = "NO CHANGE LEFT"
+
+  return bill_freq
+
+
+
+  # How do we check for the different variations in bills that can be returned?
+
+print(lemonadeChange([5,5,5,10,20]))
