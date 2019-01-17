ddcf7e7e74d9dce85e206cec350cb59d59bb1853
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-17 12:59:46 -0800

    Have to optimize for run time and space havent yet analyzed

--- day6.py
+++ day6.py
@@ -4,17 +4,79 @@ def lengthOfLongestSubstring(self, s):
         :rtype: int
         """
         unique_char = set()
-        backtracing_index = 0
-        
-        # WHAT ARE THE MOST BASIC TRUTHS ABOUT THIS PROBLEM AWAY FROM CODING
-        for index,letter in enumerate(s):
-            if letter not in unique_char:
-                unique_char.add(letter)
-            else:
-                # START NEW SEQUENCE bc you know repeating character right behind it not neccasarily true for example "bcdbg" # The sequence repeats at b but b isn't neccesarily behind it so we have to back track to the index of the previous time that element occured plus 1
-                unique_char.clear
-                backtracing_index = index
-                while index != 0:
-                    if s[backtracing_index] == letter:
-                        # Found the previous occurence of the repeating letter have to add next letter to set and then start the loop from there so we need two while loops instead of one for loop and one while loop
+        backtracing_index =  # Set to current index and works backward when iterating
+        current_index = 0
+        done_rewinding = False # FLAG TO MARK WE FOUND PREVIOUS REPEATED ELEMENT
+        counter = 0
+
+        # # WHAT ARE THE MOST BASIC TRUTHS ABOUT THIS PROBLEM AWAY FROM CODING
+        # for index,letter in enumerate(s):
+        #     if letter not in unique_char:
+        #         unique_char.add(letter)
+        #     else:
+        #         # START NEW SEQUENCE bc you know repeating character right behind it not neccasarily true for example "bcdbg" # The sequence repeats at b but b isn't neccesarily behind it so we have to back track to the index of the previous time that element occured plus 1
+        #         unique_char.clear
+        #         backtracing_index = index
+        #         while index != 0:
+        #             if s[backtracing_index] == letter:
+        #                 # Found the previous occurence of the repeating letter have to add next letter to set and then start the loop from there so we need two while loops instead of one for loop and one while loop
                         
+        while index <= len(s):
+            current_letter = s[index]
+            if current_letter not in unique_char:
+                unique_char.add(current_letter)
+
+            # Meaning we found a repeating element
+            elif current_letter in unique_char: 
+                print("SET: %s" %(unique_char))
+                unique_char.clear() # CLEAR THE SET TO START NEW SEQUENCY
+                while not done_rewinding:
+                    backtracing_index = index
+                    if s[backtracing_index] == letter:
+                        index = backtracing_index # If you found the previous repeated element then start while loop on first unique character you see again
+                    backtracing_index -= 1# Keep rewinding back
+            index += 1
+        
+
+
+def lengthOfLongestSubstring(s):
+  """
+  :type s: str
+  :rtype: int
+  """
+  unique_char = set()
+  backtracing_index = 0  # Set to current index and works backward when iterating
+  current_index = 0
+  done_rewinding = False
+  counter = 0
+  output = []
+                  
+  while current_index < len(s):
+
+      current_letter = s[current_index]
+      if current_letter not in unique_char:
+          unique_char.add(current_letter)
+          counter += 1
+
+      # Meaning we found a repeating element
+      else: 
+
+          output.append(counter)
+          print(unique_char)
+          unique_char.clear()
+          counter = 0
+          backtracing_index = current_index
+
+          while not done_rewinding:
+
+            backtracing_index -= 1
+            if s[backtracing_index] == current_letter:
+              done_rewinding = True
+              current_index = backtracing_index
+
+      current_index += 1
+
+  return max(output)
+
+    
+print(lengthOfLongestSubstring("ABDEFGABEF"))
