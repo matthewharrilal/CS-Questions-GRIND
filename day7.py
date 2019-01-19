0c21f660f36824e8f8bb1a3a962cbac108ac6202
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-18 22:58:41 -0800

    Completed isValid challenge on leetcode have to analyze runtime

--- day7.py
+++ day7.py
@@ -17,3 +17,43 @@ def intersection(self, nums1, nums2):
                 output.append(num)
                 
         return output
+
+
+def isValid(self, s):
+        
+        
+        """
+        :type s: str
+        :rtype: bool
+        """
+        punc_openers = {"(": ")", "[":"]", "{": "}"}
+        punc_closers = {")": "(", "]":"[", "}": "{"}
+        opened_amount = 0
+
+        punc_stack = []
+
+        if s == "":
+             return True
+        if len(s) == 1:
+            return False
+        
+        if s[0] not in punc_openers.keys(): # MEANING STRING STARTED OFF WITH CLOSED LETTER AUTOMATICALLY MAKING INVALID
+              return False
+
+        for index, letter in enumerate(s):
+            if letter in punc_openers: # If letter is an opener then append it to the stack
+                punc_stack.append(letter)
+                opened_amount += 1
+
+            elif letter in punc_closers and punc_stack: # If letter is a closer then check if can be resolved in stack
+
+                if punc_closers[letter] == punc_stack[len(punc_stack) - 1]:
+                    punc_stack.pop(len(punc_stack) - 1)
+                    opened_amount -= 1
+
+                else:
+                    return False
+            else:
+                return False
+
+        return (opened_amount == 0)
