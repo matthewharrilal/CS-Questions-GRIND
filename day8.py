da37388a8229a8e30b5a47da8df65bdd8617c0e8
Matthew Harrilal <matthewharrilal@gmail.com>
2019-01-23 17:03:01 -0800

    Dedicated to sorting algorithms

---
+++ day8.py
@@ -0,0 +1,7 @@
+# DEDICATING TO SORTED ALGORITHMS
+
+'''Selection sort what is the runtime and inefficiences you see with the sorting algorithm after you implement it'''
+
+
+def selection_sort(unsorted_arr):
+    
