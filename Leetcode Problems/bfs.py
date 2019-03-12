845ebf14473162a864b825c2710032a985db9129
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-12 11:19:53 -0700

    Finsihed level order traversal need more practice with traversals

--- Leetcode Problems/bfs.py
+++ Leetcode Problems/bfs.py
@@ -12,7 +12,6 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         queue_nodes = [] # Used to keep track of the traversal
-        done = False # Stops under the condition that we have traversed all nodes
         output = [] # Picks up elements on the way
         
         
@@ -22,22 +21,26 @@ class Solution(object):
             
         
         while len(queue_nodes) > 0:
-            current_node = queue_nodes.pop(0) # So we are appending the left and right child
             level = []
             
-            if current_node is not None:
-                                
-                if current_node.left: # You know when you append children you are going to go a level deeper
+            print(len(queue_nodes), queue_nodes)
+            for _ in range(len(queue_nodes)): # Want to traverse through each node's children on a level
+                
+                current_node = queue_nodes.pop(0) # So we are appending the left and right child
+                
+                if current_node is not None:
                     
-                    level.append(current_node.left.val)
-                    queue_nodes.append(current_node.left)
 
-                if current_node.right:
-                    
-                    level.append(current_node.right.val)
-                    queue_nodes.append(current_node.right)
-                    
-                if level: # Meaning that there were valid nodes present on the level
-                    output.append(level)
+                    if current_node.left: # You know when you append children you are going to go a level deeper
+                        level.append(current_node.left.val)
+                        queue_nodes.append(current_node.left)
+
+                    if current_node.right:
+
+                        level.append(current_node.right.val)
+                        queue_nodes.append(current_node.right)
+
+            if level: # Meaning that there were valid nodes present on the level
+                output.append(level)
             
         return output
