bdbd53b95daccec80f1d333ca84749ee830da8dc
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-13 11:09:41 -0700

    Implement find parent node method and the insert method

--- Leetcode Problems/convert_bst_doubly_linked_list.py
+++ Leetcode Problems/convert_bst_doubly_linked_list.py
@@ -13,21 +13,42 @@ class BinarySearchTree(object):
     
     def insert(self, item):
 
+        new_node = BinaryTreeNode(item)
         
         if self.root is None:
             self.root = BinaryTreeNode(item)
             return
+
+        parent_node = self.find_parent_node(item): # Going to pass by value ... still alters just a copy
+
+        if item <= parent_node.data:
+            parent_node.left = new_node
+        
+        else:
+            parent_node.right = new_node
         
     
     def find_parent_node(self, item):
         '''Finds parent node of given item'''
-        current_node = self.root 
+        current_node, parent_node = self.root, None
 
         if current_node.data == item: 
             return None # No parent node of the root node
 
-        if cur
+        while current_node is not None:
 
+
+            if item <= current_node.data: # If item is smaller then root traverse left
+                parent_node = current_node
+                current_node = current_node.left
+
+            else: # If item is bigger than root then traverse right
+                current_node = current_node.right
+
+            if current_node.data == item:
+                return parent_node
+
+        # Not checking for the case if BST doesn't contain the item at all ... assuming all nodes are valid for the meanwhile
         
 
 class ListNode(object):
