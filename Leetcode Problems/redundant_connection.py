9a661d7110e6765e9787e3146969fb1fb302be2d
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-18 18:48:45 -0700

    Implemented solution for 1 degree of separation

--- Leetcode Problems/redundant_connection.py
+++ Leetcode Problems/redundant_connection.py
@@ -19,3 +19,32 @@ class Solution(object):
                 
         return None
         
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        
+        adjacency_list = [[] for _ in range(len(edges) + 1)]
+        
+        for pair in edges:
+            u, v = pair[0], pair[1]
+            
+            u_index = u - 1
+            v_index = v - 1
+            
+            adjacency_list[u_index].append(v)
+            
+            adjacency_list[v_index].append(u)
+            
+            if len(adjacency_list[u_index]) > 1:
+                for vertex in adjacency_list[u_index]:
+                    if v in adjacency_list[vertex - 1]:
+                        return [u, v]
+                        
+            
+            
+                
+        return None
+        
