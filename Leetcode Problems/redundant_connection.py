b5f0acf7008ade355ef3e7b6e74b5a3c7340aca6
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-19 07:25:50 -0700

    Implemented function for redundant connection using custom implementation of union and find

--- Leetcode Problems/redundant_connection.py
+++ Leetcode Problems/redundant_connection.py
@@ -1,50 +1,47 @@
 class Solution(object):
+    
+    # A utility function to do union of two subsets 
+    def union(self,parent,u,v): 
+        u_set = self.findParentSubset(parent, u) 
+        v_set = self.findParentSubset(parent, v) 
+        parent[u_set] = v_set 
+    
+    def findParentSubset(self, parent, vertex):
+        if parent[vertex] == -1: 
+            return vertex
+        
+        if parent[vertex]!= -1: 
+            # print("In here")
+            return self.findParentSubset(parent,parent[vertex]) 
+            
+    
     def findRedundantConnection(self, edges):
         """
         :type edges: List[List[int]]
         :rtype: List[int]
         """
         
-        adjacency_list = [[] for _ in range(len(edges) + 1)]
+        parent = [-1] * len(edges) # Single element sets no edges between vertices yet
         
         for pair in edges:
-            u, v = pair[0], pair[1]
+            u,v = pair[0], pair[1]
             
-            adjacency_list[u - 1].append(v)
+            # Need -1 of vertices because dealing with list indices
+            u_subset = self.findParentSubset(parent, u - 1)
+            v_subset = self.findParentSubset(parent, v - 1)
             
-            if len(adjacency_list[v - 1]) > 0:
+            # if u == 1:
+            #     print("SUbset that 1 belongs to ", u_subset)
+            
+            # If elements lie in the same subset
+            if u_subset == v_subset:
                 return [u,v]
-            else:
-                adjacency_list[v - 1].append(v)
-                
-        return None
+            
+            else: # Create edge between two subsets
+                self.union(parent, u - 1, v - 1)
         
-class Solution(object):
-    def findRedundantConnection(self, edges):
-        """
-        :type edges: List[List[int]]
-        :rtype: List[int]
-        """
+
+            
+            
         
-        adjacency_list = [[] for _ in range(len(edges) + 1)]
-        
-        for pair in edges:
-            u, v = pair[0], pair[1]
-            
-            u_index = u - 1
-            v_index = v - 1
-            
-            adjacency_list[u_index].append(v)
-            
-            adjacency_list[v_index].append(u)
-            
-            if len(adjacency_list[u_index]) > 1:
-                for vertex in adjacency_list[u_index]:
-                    if v in adjacency_list[vertex - 1]:
-                        return [u, v]
-                        
-            
-            
-                
-        return None
         
