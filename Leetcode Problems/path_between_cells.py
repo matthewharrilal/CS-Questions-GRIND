2522fc7b526f09c8c48736bfbca62c93acf08119
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-27 17:08:18 -0700

    Able to find the source and destination points

---
+++ Leetcode Problems/path_between_cells.py
@@ -0,0 +1,37 @@
+# Find a path between cells
+
+# 1 -> Marks the source
+# 2 -> Marks the destination
+# 3 -> Marks a blank cell
+# 4 -> Marks a blank wall
+
+# Can only traverse through the blank cells
+
+# Given the matrix [
+# [0,3,2]
+# [3,3,0]
+# [1,3,0]
+# ]  return true if there is a valid pathway from source to destination
+
+
+def find_valid_pathway(matrix):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    source = None
+    destination = None
+
+    # The first task is to find the source point position and return the coordinates of that point 
+    for row_index in range(rows):
+        for column_index in range(columns):
+            current_element = matrix[row_index][column_index]
+
+            if current_element == 1:  # Meaning that we found the source
+                source = (row_index,column_index)
+
+            if current_element == 2:  # Meaning we found the destination
+                destination = (row_index, column_index)
+
+    return source, destination
+
+matrix = [[0,3,2], [3,3,0], [1,3,0]]
+print(find_valid_pathway(matrix))
