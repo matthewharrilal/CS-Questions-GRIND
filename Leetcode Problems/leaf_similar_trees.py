f0c8b5c80a8e2721e4658941b10990382a0433bc
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-01 19:17:29 -0700

    Not solving case of [1,2] and [2,2]

--- Leetcode Problems/leaf_similar_trees.py
+++ Leetcode Problems/leaf_similar_trees.py
@@ -6,33 +6,23 @@
 #         self.right = None
 
 class Solution(object):
-    def leafSimilar(self, root1, root2):
-        """
-        :type root1: TreeNode
-        :type root2: TreeNode
-        :rtype: bool
-        """
-        
-        root1_sequence, root2_sequence = [], []
+    
+    def inorderTraversal(self, root):
+        root_sequence = []
         stack = []
         done = False
         
-        # Have to find a way to extract leaf sequence from binary tree
-        
-        
         # Inorder traversal 
         while not done:
-            while root1 is not None:
+            while root is not None:
                 
                 # Add root to the stack for later processing
-                stack.append(root1)
-                print(root1.val)
-                root1 = root1.left
+                stack.append(root)
+                root = root.left
                 
             # If there are no more nodes to traverse before we pop from the stack to process
             if len(stack) == 0:
-                print("HERE ", root1_sequence)
-                return root1_sequence
+                return root_sequence
             
             current_node = stack.pop(len(stack)  - 1)  # Pop the topmost element in the stack
             
@@ -40,6 +30,28 @@ class Solution(object):
             # We know that the left child is null and right is null we then hit a leaf node
             if current_node.right is None:
                 
-                root1_sequence.append(current_node.val)
+                root_sequence.append(current_node.val)
             
-            root1 = current_node.right
+            root = current_node.right
+        
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        
+        root1_sequence = self.inorderTraversal(root1)
+        root2_sequence = self.inorderTraversal(root2)
+        
+        print(root1_sequence, root2_sequence)
+        
+        first_counter, second_counter = 0, 0 
+        
+        while first_counter < len(root1_sequence) and second_counter < len(root2_sequence):
+            if root1_sequence[first_counter] != root2_sequence[second_counter]:
+                return False
+            
+            first_counter += 1
+            second_counter += 1
+        return True
