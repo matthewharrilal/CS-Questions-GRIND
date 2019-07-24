5ad4a5bdef799384e132384d95ef9d08c5f29277
Matthew Harrilal <matthewharrilal@gmail.com>
2019-07-23 21:07:14 -0700

    Added an alternative solution to the island perimeter method that takes more time but is cleaner in exectuion

--- New LeetCode Problems/island_perimeter.py
+++ New LeetCode Problems/island_perimeter.py
@@ -42,3 +42,67 @@ class Solution(object):
                 total_perimeter = self.dfs(grid, neighbor_row, neighbor_column, visited, total_perimeter)
 
         return total_perimeter
+
+
+
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        rows = len(grid)
+        columns = len(grid[0])
+        
+        for row in range(rows):
+            for column in range(columns):
+                if grid[row][column] == 1:
+                    # print("STARTING OVER")
+                    perimeter = self.dfs(grid, row, column)
+                    
+                    return perimeter
+        
+    def dfs(self, grid, row,  column, visited=None, total_perimeter=None):
+         
+        if visited is None:
+            visited = set()
+            total_perimeter = 0
+            
+        if (0 <= row < len(grid)) == False or (0 <= column < len(grid[0])) == False:
+            total_perimeter += 1
+            return total_perimeter
+        
+        if grid[row][column] == 0:
+            total_perimeter += 1
+            return total_perimeter
+        
+    
+        if (row, column) in visited:
+            return total_perimeter
+        
+        
+        # if 0 <= row < len(grid) and 0 <= column < len(grid[0]) and (row, column) not in visited and grid[row][column] == 1:
+        visited.add((row, column))
+        total_perimeter = self.dfs(grid, row + 1, column, visited, total_perimeter)
+        total_perimeter = self.dfs(grid, row - 1, column, visited, total_perimeter)
+        total_perimeter = self.dfs(grid, row, column + 1, visited, total_perimeter)
+        total_perimeter = self.dfs(grid, row, column - 1, visited, total_perimeter)
+
+#         for neighbor_row, neighbor_column in [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]:
+#             # print(row, column)
+#             if neighbor_row < 0 or neighbor_row >= len(grid) or neighbor_column < 0 or neighbor_column >= len(grid[0]):
+#                 total_perimeter += 1
+             
+#             elif grid[neighbor_row][neighbor_column] == 0:
+#                 total_perimeter += 1
+
+            
+#             elif grid[neighbor_row][neighbor_column] == 1:  
+#                 visited.add((row, column))
+#                 total_perimeter = self.dfs(grid, neighbor_row, neighbor_column, visited, total_perimeter)
+
+
+
+        return total_perimeter
